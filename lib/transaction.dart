5a47c7032a20542c1c3c246f6a6c6e39116bd06a
Reza Salehipour <rez.salehipour@gmail.com>
2022-12-29 10:10:14 +0000

    add transaction class to work with properties

---
+++ lib/transaction.dart
@@ -0,0 +1,16 @@
+// @dart=2.9
+
+import 'package:flutter/foundation.dart';
+
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction(
+      {@required this.id,
+      @required this.title,
+      @required this.amount,
+      @required this.date});
+}
