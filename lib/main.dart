8e0862d74987fd26b050a3215003775bc28ba0e9
Reza Salehipour <rez.salehipour@gmail.com>
2023-01-04 15:39:16 +0000

    change the theme font of project

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expenses',
+      theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple)
+              .copyWith(secondary: Colors.amber),
+          fontFamily: 'Quicksand'),
+      //    theme: ThemeData(primarySwatch: Colors.purple,
+      // accentColor: Colors.amber),
       home: MyHomePage(),
     );
   }
@@ -61,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter App'),
+        title: Text('Personal Expenses'),
         actions: <Widget>[
           IconButton(
               icon: Icon(Icons.add),
