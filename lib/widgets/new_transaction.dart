d4fda8104ace0bb2b31c782c60b5f0bb22a441a6
Reza Salehipour <rez.salehipour@gmail.com>
2023-01-04 15:20:00 +0000

    change to statefull class & add navigator to pop when submit

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,13 +1,20 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_constructors_in_immutables, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
 
-class NewTransaction extends StatelessWidget {
+class NewTransaction extends StatefulWidget {
   final Function addTransaction;
-  final titleController = TextEditingController();
-  final amountController = TextEditingController();
 
   NewTransaction(this.addTransaction);
+
+  @override
+  State<NewTransaction> createState() => _NewTransactionState();
+}
+
+class _NewTransactionState extends State<NewTransaction> {
+  final titleController = TextEditingController();
+
+  final amountController = TextEditingController();
 
   void submitData() {
     final enteredTitle = titleController.text;
@@ -17,7 +24,10 @@ class NewTransaction extends StatelessWidget {
       return;
     }
 
-    addTransaction(titleController.text, double.parse(amountController.text));
+    widget.addTransaction(
+        titleController.text, double.parse(amountController.text));
+
+    Navigator.of(context).pop();
   }
 
   @override
